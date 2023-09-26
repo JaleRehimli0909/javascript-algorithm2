5a450d0c323fe8facdd4eb8ca8d7f28a76160fb4
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-26 22:39:24 +0400

    Task2 added(numbers, sum, sqrt)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-// TASK1
+/*
+TASK1
 var count = 0;
 var S = 0;
-for(let i = 1; i <= 10; i++){
+for(let i = 1; i <= 1000; i++){
   if(i%5 === 0 && i%7 !== 0){
     S+=i
     count++;
@@ -11,3 +12,15 @@ for(let i = 1; i <= 10; i++){
 }
 console.log('Sayi:',count);
 console.log('Sum:',S);
+*/
+// Task2
+
+var num1 = Number(prompt("Enter num1.."));
+var num2 = Number(prompt("Enter num2.."));
+var num3 = Number(prompt("Enter num3.."));
+
+var sum = num1 + num2 + num3;
+console.log(num1,'+', num2, '+',num3, '=', sum);
+var sqrt = sum*sum;
+console.log('Sum-in kvadrati:', sqrt);
+
